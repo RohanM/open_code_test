c9ae6a2428311f2697dd925afb90fef6dada8bdb
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-08 14:49:56 +1000

    New Rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCodeTest::Application.config.secret_token = '90fbcb82c8b68d4e4855984acdd88a0439d6261eececfd9bd5c4437eb326e2884d321deaec1e8f56868854568058fd39c7568670949ece0e6e9e17b3a674173d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OpenCodeTest::Application.config.session_store :cookie_store, key: '_open_code_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OpenCodeTest::Application.config.session_store :active_record_store
